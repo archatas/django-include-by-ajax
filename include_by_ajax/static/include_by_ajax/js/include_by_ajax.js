c3d1b3080976222b1e8cf310eb770557e74c038d
Aidas Bendoraitis <aidasbend@yahoo.com>
2018-08-10 06:48:03 +0200

    ✨ Initial prototype

--- include_by_ajax/static/include_by_ajax/js/include_by_ajax.js
+++ include_by_ajax/static/include_by_ajax/js/include_by_ajax.js
@@ -1,8 +1,24 @@
-jQuery(function() {
-    // TO DO:
+jQuery(function($) {
     // 1. Check if there are placeholders
+    let placeholder_count = $('.ajax-placeholder').length;
+    if (!placeholder_count) {
+        return;
+    }
     // 2. If yes, then load the same page with full_render=1 query parameter again by Ajax
-    // 3. Onload parse the HTML as jQuery object
-    // 4. For each placeholder in that jQuery object, populate the placeholder in the document with placeholder's content
-    // 5. Trigger a special event "all_loaded"
+    let url = location.href;
+    if (url.indexOf('?') === -1) {
+        url += '?include_by_ajax_full_render=1';
+    } else {
+        url += '&include_by_ajax_full_render=1';
+    }
+    // 3. For each placeholder in that jQuery object, populate the placeholder in the document with placeholder's content
+    $('.ajax-placeholder').each(function(index, element) {
+        $(this).load(url + ' .ajax-placeholder:eq(' + index + ')>', function() {
+            placeholder_count --;
+            if (!placeholder_count) {
+                // 4. Trigger a special event "all_loaded"
+                $(document).trigger('include_by_ajax_all_loaded');
+            }
+        });
+    });
 });
--- include_by_ajax/templatetags/include_by_ajax_tags.py
+++ include_by_ajax/templatetags/include_by_ajax_tags.py
@@ -7,6 +7,7 @@ register = template.Library()
 @register.inclusion_tag('include_by_ajax/includes/placeholder.html', takes_context=True)
 def include_by_ajax(context, template_name):
     request = context['request']
-    # request.is_ajax() and request.GET.get('full_render')
+    if request.is_ajax() and request.GET.get('include_by_ajax_full_render'):
+        context['include_by_ajax_full_render'] = template_name
     context['template_name'] = template_name
     return context
