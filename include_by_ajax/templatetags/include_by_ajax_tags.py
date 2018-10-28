0e6874d55e42488ae3ce61ec6b1133e8eed22cc0
Aidas Bendoraitis <aidasbend@yahoo.com>
2018-10-28 04:07:24 +0100

    ✨ Unit tests.

--- include_by_ajax/templatetags/include_by_ajax_tags.py
+++ include_by_ajax/templatetags/include_by_ajax_tags.py
@@ -12,12 +12,14 @@ def include_by_ajax(context, template_name):
     app_config = apps.get_app_config('include_by_ajax')
     # get User-Agent
     request = context['request']
-    user_agent = request.META['HTTP_USER_AGENT']
+    user_agent = request.META.get('HTTP_USER_AGENT') or ''
     # check if the current request is coming from a web crawler
     is_web_crawler = bool(app_config.web_crawler_pattern.search(user_agent))
     # in case of web crawler or an Ajax call we'll do full render
-    if is_web_crawler or request.is_ajax() and request.GET.get('include_by_ajax_full_render'):
-        context['include_by_ajax_full_render'] = bool(template_name)
+    context['include_by_ajax_full_render'] = bool(
+        is_web_crawler or
+        request.is_ajax() and request.GET.get('include_by_ajax_full_render')
+    )
     # in case of web crawler, the placeholder shouldn't be wrapped with a <section>
     context['include_by_ajax_no_placeholder_wrapping'] = is_web_crawler
     # pass down the template path
--- include_by_ajax/tests/test_templatetags.py
+++ include_by_ajax/tests/test_templatetags.py
@@ -2,4 +2,42 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.test.client import RequestFactory
 
+from include_by_ajax.templatetags.include_by_ajax_tags import include_by_ajax
+
+
+class IncludeByAjaxTemplateTagTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.factory = RequestFactory()
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_first_access(self):
+        request = self.factory.get('/')
+        template_name = "included.html"
+        context = include_by_ajax(context={'request': request}, template_name=template_name)
+        self.assertEquals(context['include_by_ajax_no_placeholder_wrapping'], False)
+        self.assertEquals(context['include_by_ajax_full_render'], False)
+        self.assertEquals(context['template_name'], template_name)
+
+    def test_access_by_ajax(self):
+        request = self.factory.get('/', {
+            'include_by_ajax_full_render': '1',
+        }, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        template_name = "included.html"
+        context = include_by_ajax(context={'request': request}, template_name=template_name)
+        self.assertEquals(context['include_by_ajax_no_placeholder_wrapping'], False)
+        self.assertEquals(context['include_by_ajax_full_render'], True)
+        self.assertEquals(context['template_name'], template_name)
+
+    def test_web_crawler_access(self):
+        request = self.factory.get('/', HTTP_USER_AGENT='Googlebot')
+        template_name = "included.html"
+        context = include_by_ajax(context={'request': request}, template_name=template_name)
+        self.assertEquals(context['include_by_ajax_no_placeholder_wrapping'], True)
+        self.assertEquals(context['include_by_ajax_full_render'], True)
+        self.assertEquals(context['template_name'], template_name)
