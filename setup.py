1940719e6574fb29abae762a65d6627b2201d4f9
Aidas Bendoraitis <aidasbend@yahoo.com>
2020-09-23 00:20:50 +0200

    Bump version: 2.0.0 → 2.1.0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     name='django-include-by-ajax',
     packages=find_packages(include=['include_by_ajax']),
     url='https://github.com/archatas/django-include-by-ajax',
-    version='2.0.0',
+    version='2.1.0',
     zip_safe=False,
 )
