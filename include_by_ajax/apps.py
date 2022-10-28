8ea5155e05405e0c6b6e8dfded1b15cf0b3e255b
Aidas Bendoraitis <aidasbend@yahoo.com>
2022-10-28 09:07:23 +0200

    🔨 Only webcrawlers without JavaScript support are treated differently.

--- include_by_ajax/apps.py
+++ include_by_ajax/apps.py
@@ -9,21 +9,26 @@ from django.conf import settings
 
 
 class IncludeByAjaxConfig(AppConfig):
-    name = 'include_by_ajax'
+    name = "include_by_ajax"
     verbose_name = _("Include by Ajax")
 
-    WEB_CRAWLERS = getattr(settings, 'INCLUDE_BY_AJAX_WEB_CRAWLERS', (
-        'Googlebot', # Google bot
-        'Bingbot', # Bing bot
-        'Slurp', # Yahoo! bot
-        'DuckDuckBot', # Duck Duck Go bot
-        'Baiduspider', # Baidu bot
-        'YandexBot', # Yandex bot
-        'Sogou', # Sogou bot
-        'Exabot', # Exalead bot
-        'ia_archiver', # Alexa bot
-    ))
-    web_crawler_pattern = re.compile('|'.join(sorted(WEB_CRAWLERS, reverse=True)), flags=re.IGNORECASE)
+    WEB_CRAWLERS_WITHOUT_JS = getattr(
+        settings,
+        "INCLUDE_BY_AJAX_WEB_CRAWLERS",
+        (
+            "Bingbot",  # Bing bot
+            "Slurp",  # Yahoo! bot
+            "DuckDuckBot",  # Duck Duck Go bot
+            "Baiduspider",  # Baidu bot
+            "YandexBot",  # Yandex bot
+            "Sogou",  # Sogou bot
+            "Exabot",  # Exalead bot
+            "ia_archiver",  # Alexa bot
+        ),
+    )
+    web_crawler_pattern = re.compile(
+        "|".join(sorted(WEB_CRAWLERS_WITHOUT_JS, reverse=True)), flags=re.IGNORECASE
+    )
 
     def ready(self):
         pass
